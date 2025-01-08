f67c13d18d5eb87308bc3197017db8c5716779b6
dldlscjf123 <dldlscjf1231@naver.com>
2025-01-08 15:00:31 +0900

    feat : UserRepository추가

---
+++ src/main/java/com/lee/drugescape/drugescape/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.lee.drugescape.drugescape.repository;
+
+import com.lee.drugescape.drugescape.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUserId(String userId);
+
+}
