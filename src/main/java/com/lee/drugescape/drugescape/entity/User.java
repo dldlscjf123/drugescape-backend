4feb20c79eebe0622e2f449a55a54df38d04a6df
dldlscjf123 <dldlscjf1231@naver.com>
2025-01-08 14:49:17 +0900

    feat : user 엔티티 추가

---
+++ src/main/java/com/lee/drugescape/drugescape/entity/User.java
@@ -0,0 +1,37 @@
+package com.lee.drugescape.drugescape.entity;
+
+import com.lee.drugescape.drugescape.type.UserType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "member")
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 50)
+    private String userId;
+
+    @Column(nullable = false)
+    private String userName;
+
+    @Column(nullable = false)
+    private String userEmail;
+
+    @Column
+    private String picture;
+
+    @Column(columnDefinition = "ENUM('GOOGLE') DEFAULT 'GOOGLE'")
+    @Enumerated(EnumType.STRING)
+    private UserType userType;
+
+}
