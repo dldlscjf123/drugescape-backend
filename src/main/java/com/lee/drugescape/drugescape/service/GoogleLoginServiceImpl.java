1174158547f3f519122626acda62087f542cb370
dldlscjf123 <dldlscjf1231@naver.com>
2025-01-08 14:58:50 +0900

    feat : GoogleLoginServiceImpl추가 SocialLoginService구현체로 구글로그인으로 로그인했을경우 로직

---
+++ src/main/java/com/lee/drugescape/drugescape/service/GoogleLoginServiceImpl.java
@@ -0,0 +1,100 @@
+package com.lee.drugescape.drugescape.service;
+
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.lee.drugescape.drugescape.dto.GoogleLoginResponse;
+import com.lee.drugescape.drugescape.dto.GoogleRequestAccessTokenDto;
+import com.lee.drugescape.drugescape.dto.SocialAuthResponse;
+import com.lee.drugescape.drugescape.dto.SocialUserResponse;
+import com.lee.drugescape.drugescape.feign.google.GoogleAuthApi;
+import com.lee.drugescape.drugescape.feign.google.GoogleUserApi;
+import com.lee.drugescape.drugescape.type.UserType;
+import com.lee.drugescape.drugescape.util.GsonLocalDateTimeAdapter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@Qualifier("googleLogin")
+public class GoogleLoginServiceImpl implements SocialLoginService {
+    private final GoogleAuthApi googleAuthApi;
+    private final GoogleUserApi googleUserApi;
+
+    @Value("${spring.security.oauth2.client.registration.google.client-id}")
+    private String googleAppKey;
+    @Value("${spring.security.oauth2.client.registration.google.client-secret}")
+    private String googleAppSecret;
+    @Value("${spring.security.oauth2.client.registration.google.redirect-uri}")
+    private String googleRedirectUri;
+    @Value("${spring.security.oauth2.client.registration.google.authorization-grant-type}")
+    private String googleGrantType;
+
+    @Override
+    public UserType getServiceName() {
+        return UserType.GOOGLE;
+    }
+
+    @Override
+    public SocialAuthResponse getAccessToken(String authorizationCode) {
+       String decodedCode = decodeCode(authorizationCode);
+        GoogleRequestAccessTokenDto requestAccessTokenDto = GoogleRequestAccessTokenDto.builder()
+                .code(decodedCode)
+                .client_id(googleAppKey)
+                .client_secret(googleAppSecret)
+                .redirect_uri(googleRedirectUri)
+                .grant_type(googleGrantType)
+                .build();
+        // 로그 추가 - GoogleRequestAccessTokenDto 값 확인
+        log.info("Request Access Token DTO: code={}, client_id={}, client_secret={}, redirect_uri={}, grant_type={}",
+                requestAccessTokenDto.getCode(),
+                requestAccessTokenDto.getClient_id(),
+                requestAccessTokenDto.getClient_secret(),
+                requestAccessTokenDto.getRedirect_uri(),
+                requestAccessTokenDto.getGrant_type());
+
+        ResponseEntity<?> response = googleAuthApi.getAccessToken(requestAccessTokenDto);
+        log.info("API Response Body: {}", response.getBody());
+
+        return new Gson()
+                .fromJson(
+                        response.getBody().toString(),
+                        SocialAuthResponse.class
+                );
+    }
+
+    @Override
+    public SocialUserResponse getUserInfo(String accessToken) {
+        ResponseEntity<?> response = googleUserApi.getUserInfo(accessToken);
+
+        log.info("google user response");
+        log.info(response.toString());
+
+        String jsonString = response.getBody().toString();
+
+        Gson gson = new GsonBuilder()
+                .setPrettyPrinting()
+                .registerTypeAdapter(LocalDateTime.class, new GsonLocalDateTimeAdapter())
+                .create();
+
+        GoogleLoginResponse googleLoginResponse = gson.fromJson(jsonString, GoogleLoginResponse.class);
+
+        return SocialUserResponse.builder()
+                .id(googleLoginResponse.getId())
+                .email(googleLoginResponse.getEmail())
+                .build();
+    }
+    private String decodeCode(String encodedCode) {
+        return URLDecoder.decode(encodedCode, StandardCharsets.UTF_8);
+    }
+}
