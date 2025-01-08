97b49aa5afc3380949573d9140b16c7a0d19153a
dldlscjf123 <dldlscjf1231@naver.com>
2025-01-08 15:17:59 +0900

    feat: LoginResponse 추가

---
+++ src/main/java/com/lee/drugescape/drugescape/dto/LoginResponse.java
@@ -0,0 +1,15 @@
+package com.lee.drugescape.drugescape.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class LoginResponse {
+    private Long id;
+}
