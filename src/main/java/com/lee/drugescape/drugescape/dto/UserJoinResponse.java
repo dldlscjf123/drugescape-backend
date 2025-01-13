90a84728b8b325682cfa4b24dc0d7982e75ee2c8
dldlscjf123 <dldlscjf1231@naver.com>
2025-01-13 11:16:30 +0900

    feat : UserJoinResponse 추가 db저장후 리턴 dto

---
+++ src/main/java/com/lee/drugescape/drugescape/dto/UserJoinResponse.java
@@ -0,0 +1,14 @@
+package com.lee.drugescape.drugescape.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class UserJoinResponse {
+    private Long id;
+}
